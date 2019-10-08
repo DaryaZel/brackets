52410d751c490b7977ee2ee34bd194a93b9a748f
Darya Zelenkova <daryazeliankova@yandex.ru>
2019-10-09 01:37:04 +0300

    feat: implement check function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,22 @@
 module.exports = function check(str, bracketsConfig) {
-  // your solution
+    let bracketsArray = [];
+    let strArray = str.split("");
+    let newArray = [];
+    bracketsConfig.forEach(item => {
+        bracketsArray.push(item[0] + item[1])
+    })
+    for (let i = 0; i < str.length; i++) {
+        newArray.push(str[i])
+        bracketsArray.forEach(item => {
+            if ((newArray[newArray.length - 2] + newArray[newArray.length - 1]) == item) {
+                newArray = newArray.slice(0, -2)
+            }
+        })
+    }
+    if (newArray.length == 0) {
+        return true
+    }
+    else {
+        return false
+    }
 }
